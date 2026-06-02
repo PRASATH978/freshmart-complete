ef84d1f7f3eec3d002e1df3ce603595fa66bc99c
PRASATH978 <163582130+PRASATH978@users.noreply.github.com>
2026-06-02 23:40:52 +0530

    temp clear images

--- backend/veggieshop/urls.py
+++ backend/veggieshop/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from apps.products.views import clear_images
 
 
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
     path('api/products/', include('apps.products.urls')),
     path('api/orders/', include('apps.orders.urls')),
     path('api/delivery/', include('apps.delivery.urls')),
+    path('clear-images/', clear_images)
 ]
